174a03d0adf14a905952450b6b542bafa6b6b05c
therealalexolthoff <alex@1452design.com>
2024-06-19 21:56:43 -0400

    Buttons working to play game

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ let holder
 
 // Creates all required strings and conditions
 function createConstants(playerName) {
-    const CHOICES = ["Nil", "Rock", "Paper", "Scissors"]
+    const CHOICES = ["Rock", "Paper", "Scissors"]
     const STATES = [`${playerName} wins!`, "It's a tie.", "Computer Wins!" ]
     const RESULTS = {"Rock": 
         {"Paper": 
@@ -28,10 +28,9 @@ function createConstants(playerName) {
     }
     return [CHOICES,STATES,RESULTS]
 }
-function firstPlaythrough() {
-   
-    return createConstants(playerName)
-}
+
+// Event listeners to control the flow of the experience
+
 
 document.getElementById('saveName').addEventListener('click', () => {
     gameSection.style.display = 'block'
@@ -41,33 +40,76 @@ document.getElementById('saveName').addEventListener('click', () => {
     holder = createConstants(playerName)
 })
 
+document.getElementById('rock').addEventListener('click', ()=> {
+    playGame("Rock")
+} )
+document.getElementById('paper').addEventListener('click', ()=> {
+    playGame("Paper")
+} )
+document.getElementById('scissors').addEventListener('click', ()=> {
+    playGame("Scissors")
+} )
+
 function updateImages(image1,image2) {
     document.getElementById('playerImg').src = `./${image1}.jpg`
     document.getElementById('computerImg').src = `./${image2}.jpg`
 }
 
-function playGame() {
+function assignScore(data,states){
+    switch (data){
+        case states[0]: {
+            let playerData = document.getElementById('playerData')
+            console.log(playerData)
+            if (playerData) {
+               const newScore =  parseInt(playerData) + 1
+               playerData.innerHTML = newScore
+            }
+            else {
+                document.getElementById('playerData').innerHTML = 1
+            }
+            console.log('running')
+           let currentScore = parseInt(document.getElementById('playerData').innerHTML)
+           currentScore += 1
+           document.getElementById('playerData').innerHTML = currentScore
+        }
+        case states[2]: {
+            let computerData = document.getElementById('computerData').innerHTML
+            if (computerData) {
+               const newScore =  parseInt(computerData) + 1
+               computerData.innerHTML = newScore
+            }
+            else {
+                document.getElementById('computerData').innerHTML = 1
+            }
+            // computerScore += 1
+            
+            // document.getElementById('computerData').innerHTML = computerScore
+         }
+         default: {
+            console.log(
+                'tie'
+            )
+         }
+    }
+}
+
+function playGame(playerChoice) {
     const [CHOICES, STATES ,RESULTS] = holder
-    const randNum =  Math.floor(Math.random() * 3) 
+    const randNum =  Math.floor(Math.random() * 4) 
     const compChoice = CHOICES[randNum] 
+    console.log(compChoice)
+    console.log(RESULTS[compChoice])
     const result = RESULTS[compChoice][playerChoice]
     document.getElementById('resultText').innerText = result
-    switch (result){
-        case STATES[0]: {
-           let currentScore = parseInt(document.getElementById('playerData').innerText)
-           currentScore += 1
-           currentScore
-        }
-    }
-    
+    assignScore(result,STATES)
 }
-while (stillPlaying === true) {
+// while (stillPlaying === true) {
     
-    const randNum =  Math.floor(Math.random() * 3) 
-    const compChoice = CHOICES[randNum] 
-    const result = RESULTS[compChoice][playerChoice]
-    alert(result)
-    const playAgain = prompt("Want to play again?")
-    stillPlaying = playAgain === "yes"
-}  
+//     const randNum =  Math.floor(Math.random() * 3) 
+//     const compChoice = CHOICES[randNum] 
+//     const result = RESULTS[compChoice][playerChoice]
+//     alert(result)
+//     const playAgain = prompt("Want to play again?")
+//     stillPlaying = playAgain === "yes"
+// }  
 
